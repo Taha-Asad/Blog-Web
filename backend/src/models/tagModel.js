ce8790defa07bc15c1e09f345ce1cb44ecd7ff35
Taha-Asad <tahaasad709@gmail.com>
2025-09-13 22:05:25 +0500

    Removed Old Files

--- backend/src/models/tagModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const tagSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  slug: { type: String, unique: true },
-});
-
-module.exports = mongoose.model("Tag", tagSchema);
